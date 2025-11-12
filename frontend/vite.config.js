6b60bb1e08ce6575bb275a5124afcfcd48721c64
AdamChap94 <adambchappell94@gmail.com>
2025-11-12 16:53:16 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@root": path.resolve(__dirname, "."),
+      "@src": path.resolve(__dirname, "src"),
+    },
+  },
 });
