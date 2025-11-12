7495f56ce90e0153ae9b6d795cb544a770947229
AdamChap94 <adambchappell94@gmail.com>
2025-11-12 16:52:12 -0500

    Update arc.app.jsx

--- frontend/src/arc.app.jsx
+++ frontend/src/arc.app.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import MessageComposer from "./MessageComposer.jsx";
-import MessageRow from "./MessageRow.jsx";
+import MessageComposer from "../MessageComposer.jsx";
+import MessageRow from "../MessageRow.jsx";
 
 const API = import.meta.env.VITE_API_BASE || "";
 
