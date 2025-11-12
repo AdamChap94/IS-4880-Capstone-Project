31e266a14002dfde2c0f6f6aed2ca5cc01b7bb7f
AdamChap94 <adambchappell94@gmail.com>
2025-11-11 20:37:23 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./arc.app.jsx";
+import App from "arc.app.jsx";
 
 const container = document.getElementById("root");
 createRoot(container).render(<App />);
